04ddf2adbfdad52a2645181aeb3f1ada3aaa71d5
Nathan Hwang <thenoviceoof@gmail.com>
2012-08-17 01:01:19 -0400

    This is even slower, but whatever

--- clojure/p007.clj
+++ clojure/p007.clj
@@ -1,12 +1,14 @@
 ; find the 10001st prime
 
+(require 'clojure.contrib.seq-utils)
+
 (defn div [x y] (zero? (mod x y)))
 
 (defn gen-primes-up-to [i ind primes]
   (if (= ind (count primes))
     (last primes)
     (recur (inc i) ind
-           (if (not (empty? (filter (partial div i) primes)))
+           (if (some (partial div i) primes)
              primes
              (concat primes [i])))))
 
