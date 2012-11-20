d78280ced0d90f5715680bb2c2f20199e7236f1e
Nathan Hwang <thenoviceoof@gmail.com>
2012-11-19 23:53:46 -0800

    go p007, Using faster prime finding method

--- go/p007.go
+++ go/p007.go
@@ -7,31 +7,24 @@ import (
 	"fmt"
 )
 
-func Generate(gen chan int) {
-	for i := 2; ; i++ {
-		gen <- i
-	}
-}
-
-func Filter(prime int, ch, nch chan int) chan int {
-	for c := range ch {
-		if c%prime != 0 {
-			nch <- c
-		}
-	}
-	return nch
-}
-
 func main() {
-	ch := make(chan int)
-	go Generate(ch)
-	for i := 1; i < 10001; i++ {
-		prime := <-ch
-		// fmt.Println(prime)
-		fmt.Println(i)
-		ch1 := make(chan int)
-		go Filter(prime, ch, ch1)
-		ch = ch1
+	primes := make([]int, 10001)
+	length := 1
+	primes[0] = 2
+	i := 3
+	for ;length < 10001; {
+		prime := true
+		for j := 0; j < length; j++ {
+			if i % primes[j] == 0 {
+				prime = false
+				break
+			}
+		}
+		if prime {
+			primes[length] = i
+			length++
+		}
+		i++
 	}
-	fmt.Println(<-ch)
+	fmt.Println(primes[length-1])
 }
